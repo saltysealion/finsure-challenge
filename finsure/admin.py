d822c4ceace1c9b6094df2e4c72467ab6b7defef
Vlad's PC <vlad@kiratech.com.au>
2021-09-11 19:19:54 +1000

    added Lender model

--- finsure/admin.py
+++ finsure/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from finsure.models import Lender
 
-# Register your models here.
+
+class LenderAdmin(admin.ModelAdmin):
+    list_display = (
+        '__str__',
+        'active',
+    )
+    list_filter = ('active', )
+    search_fields = (
+        'pk',
+        'name',
+        'code',
+    )
+
+
+# REGISTER:
+
+# Lender
+admin.site.register(Lender, LenderAdmin)
+
+# Site title
+admin.site.site_header = 'Finsure Admin'
--- finsure/models.py
+++ finsure/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
-# Create your models here.
+
+class Lender(models.Model):
+    name = models.CharField(
+        max_length=50,
+        null=False,
+        blank=False,
+    )
+    code = models.CharField(
+        max_length=3,
+        null=False,
+        blank=False,
+        validators=[MinLengthValidator(limit_value=3)]
+    )
+    upfront_commission_rate = models.DecimalField(
+        max_digits=5,
+        decimal_places=3,
+        null=False,
+        blank=False,
+    )
+    trial_commission_rate = models.DecimalField(
+        max_digits=5,
+        decimal_places=3,
+        null=False,
+        blank=False,
+    )
+    active = models.BooleanField(null=False)
+
+    def __str__(self) -> str:
+        return f'{self.name} ({self.code})'
