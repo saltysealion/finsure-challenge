3d292fa759f6c60fc2a213d7ba9b9409a383092b
Vlad's PC <vlad@kiratech.com.au>
2021-09-12 22:50:29 +1000

    added loginc for import endpoint

--- api_v1/views.py
+++ api_v1/views.py
@@ -1,9 +1,12 @@
+from django.core.exceptions import ValidationError as DjangoValidationError
+from rest_framework.status import HTTP_200_OK
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.views import APIView
-from rest_framework.exceptions import NotAcceptable
+from rest_framework.exceptions import NotAcceptable, ValidationError as DRFValidationError
 from rest_framework.response import Response
 from rest_framework.settings import api_settings
 from rest_framework_csv.renderers import CSVRenderer
+from rest_framework_csv.parsers import CSVParser
 from finsure.models import Lender
 from .serializers import LenderSerializer
 
@@ -21,14 +24,53 @@ class LenderViewSet(ModelViewSet):
 
 
 class LenderImportAPIView(APIView):
+    '''
+    A custom endpoint that accepts CSV formatted data to import lenders.
+    '''
+    # Only parse CSV data at this stage
+    parser_classes = [CSVParser]
+
     def post(self, request, format=None):
-        pass
+        validated_lenders = []
+        errors = []
+        # Run validation on each record before creation;
+        # use `enumerate()` to keep track of indexes
+        for idx, record in enumerate(request.data):
+            lender = Lender(**record)
+            try:
+                lender.full_clean()
+                validated_lenders.append(lender)
+
+            except DjangoValidationError as e:
+                # As DRF JSON:API plugin doesn't provide a simple way to
+                # add `meta` data to errors, we use a little hack to provide
+                # users with more information. We remap error dict keys and
+                # prepend the index to field names so that they point to the
+                # line in the CSV that caused the error(s)
+                entry_errors = {}
+                for k, v in e.message_dict.items():
+                    # Add +1 to index since it starts with 0
+                    entry_errors[f'{idx + 1}/{k}'] = v
+                errors.append(entry_errors)
+
+        # If any errors are found, raise and stop
+        if errors:
+            raise DRFValidationError(errors)
+
+        # Otherwise bulk create validated lenders
+        created_lenders = Lender.objects.bulk_create(validated_lenders)
+
+        # Serialize created lenders to show in the response
+        result = []
+        for lender in created_lenders:
+            result.append(LenderSerializer(lender).data)
+
+        return Response(result, status=HTTP_200_OK)
 
 
 class LenderExportAPIView(APIView):
     '''
-    A custom endpoint allowing to export lenders into a CSV file.
-    Made with extendibility in mind.
+    A custom endpoint that allows exporting lenders in CSV format.
     '''
 
     # Keep JSON renderer for errors and add CSV renderer
